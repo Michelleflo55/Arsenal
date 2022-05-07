41822e8223bb917e458f701f5537fd33664c7f21
Michelle Flores <lmfg2456@gmail.com>
2022-05-06 18:35:04 -0700

    installed express

--- Arsenal-backend/server.js
+++ Arsenal-backend/server.js
@@ -1,2 +1,12 @@
 const express = require('express')
 const PORT = process.env.PORT || 3001 
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.listen(PORT, () => {
+    console.log(`Express server listening on port ${PORT}`)
+})
