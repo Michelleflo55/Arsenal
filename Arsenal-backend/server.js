3d1c74b4754129fdabfe7e51a3d131d588b50849
Michelle Flores <lmfg2456@gmail.com>
2022-05-06 19:09:42 -0700

    installed all dependencies

--- Arsenal-backend/server.js
+++ Arsenal-backend/server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const PORT = process.env.PORT || 3001 
 const cors = require('cors')
-
 const app = express()
+// const AppRouter = require('./routes/AppRouter')
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.listen(PORT, () => {
-    console.log(`Express server listening on port ${PORT}`)
-})
+
+// app.use('/api', AppRouter)
+app.get('/', (req, res) => res.json({ message: 'Server Works' }))
+app.listen(PORT, () => {console.log(`Express server listening on port ${PORT}`)})
