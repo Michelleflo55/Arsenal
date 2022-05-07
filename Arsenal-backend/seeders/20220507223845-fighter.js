8e9176a27a2c078b562a4ce3b4b7cee9b7bb302b
Michelle Flores <lmfg2456@gmail.com>
2022-05-07 15:48:36 -0700

    fixed seed skeleton

--- Arsenal-backend/seeders/20220507223845-fighter.js
+++ Arsenal-backend/seeders/20220507223845-fighter.js
@@ -1,24 +1,20 @@
 'use strict';
 
+
+
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  up: async (queryInterface, Sequelize) => {
+    const fighter = [
+
+
+
+
+
+    ]
+    await queryInterface.bulkInsert('fighters', fighter)
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: async (queryInterface, Sequelize) => {
+   await queryInterface.bulkDelete('fighters')
   }
 };
