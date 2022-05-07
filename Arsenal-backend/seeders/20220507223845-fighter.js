7e43ddd43695bbec980090b2dff57e8d35e73147
Michelle Flores <lmfg2456@gmail.com>
2022-05-07 16:01:31 -0700

    pausing fighter seed, about to create weapons model

--- Arsenal-backend/seeders/20220507223845-fighter.js
+++ Arsenal-backend/seeders/20220507223845-fighter.js
@@ -5,7 +5,13 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const fighter = [
-
+      {
+        name: '#8582',
+        image: 'https://lh3.googleusercontent.com/SeCnyjmKlM2-MWLlx7tNI_kMyki8gg4SdjGW-EY9aQk10oROiDpbkWv225zWLSI93m9MVApRxc2gzR6ZMwbq83KKMr8uQHT3Nz0kOYk=w600',
+        health: 100,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
 
 
 
