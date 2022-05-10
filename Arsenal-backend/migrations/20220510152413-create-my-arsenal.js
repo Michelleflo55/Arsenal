21e5c8c9bd960db4acb4fa9aa821dfbe5068c0fb
Michelle Flores <lmfg2456@gmail.com>
2022-05-10 08:36:43 -0700

    created my_arsenal

--- Arsenal-backend/migrations/20220510152413-create-my-arsenal.js
+++ Arsenal-backend/migrations/20220510152413-create-my-arsenal.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('My_arsenals', {
+    await queryInterface.createTable('my_arsenals', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -25,6 +25,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('My_arsenals');
+    await queryInterface.dropTable('my_arsenals');
   }
 };
--- Arsenal-backend/models/my_arsenal.js
+++ Arsenal-backend/models/my_arsenal.js
@@ -19,6 +19,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'My_arsenal',
+    tableName: 'my_arsenals'
   });
   return My_arsenal;
 };
