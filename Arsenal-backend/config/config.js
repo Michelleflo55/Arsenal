18637e5c2c3249b114768c15bd68644c4b19274c
Michelle Flores <lmfg2456@gmail.com>
2022-05-12 12:29:59 -0700

    config fix

--- Arsenal-backend/config/config.js
+++ Arsenal-backend/config/config.js
@@ -1,18 +1,21 @@
 require('dotenv').config()
- {
-  "development": {
-    "database": "Arsenal_development",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+module.exports = {
+  development: {
+    database: "Arsenal_development",
+    dialect: 'postgres'
   },
-  "test": {
-    "database": "Arsenal_test",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  test: {
+    database: "Arsenal_test",
+    dialect: 'postgres'
   },
-  "production": {
-    "database": "Arsenal_production",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+        require: true
+      }
+    }
   }
 }
