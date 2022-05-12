7496db622449588c0a6cf40d8734e07f6d22c6eb
Michelle Flores <lmfg2456@gmail.com>
2022-05-11 18:16:23 -0700

    fixing a seed

--- Arsenal-backend/routes/MyArsenalRouter.js
+++ Arsenal-backend/routes/MyArsenalRouter.js
@@ -4,4 +4,5 @@ const controller = require('../controllers/MyArsenalController')
 Router.get('/', controller.GetMyArsenal)
 Router.delete('/:player/:weapon', controller.DeleteWeaponFromArsenal)
 Router.delete('/:player', controller.DeleteMyArsenal)
+
 module.exports = Router
