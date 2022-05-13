69f3b9ec97cf3813c809810642e2952b0fc1a2eb
Michelle Flores <lmfg2456@gmail.com>
2022-05-13 14:22:48 -0700

    finished CRUD in weapon services

--- arsenal-frontend/src/services/WeaponsServices.js
+++ arsenal-frontend/src/services/WeaponsServices.js
@@ -18,13 +18,21 @@ export const GetWeapon = async (data) => {
     }
 }
 
-export const PostWeapon = async (data) => {
+export const CreateWeapon = async (data) => {
     
     try {
        const res = await Client.post(`/weapons/new/${data.playerId}/${data.choose_weaponsId}`)
        return res.data
     } catch (error) {
         throw(error)
+    }
+}
+
+export const UpdateWeapon = async (data) => {
+    try {
+        const res = await Client.put(`/weapons/${data.choose_weaponsId}`)
+    } catch (error) {
+      throw(error)  
     }
 }
 
